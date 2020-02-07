5510aeb12fd941c06604f0eecdef1eeeab521577
Rosmari Nikova <rosmari.nikova@yahoo.com>
2020-02-07 23:51:56 +0200

    Added Database models

---
+++ Exam Preparations/src/Apps/Panda/Panda.Data/DbSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Panda.Data
+{
+    public static class DbSettings
+    {
+        public const string ConnectionString = @"Server=.\SQLEXPRESS;Database=PandaDb;Trusted_Connection=True;Integrated Security=True;";
+    }
+}
---
+++ Exam Preparations/src/Apps/Panda/Panda.Data/PandaDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Panda.Data.Models;
+
+namespace Panda.Data
+{
+    public class PandaDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Package> Packages { get; set; }
+
+        public DbSet<Receipt> Receipts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(DbSettings.ConnectionString);
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
